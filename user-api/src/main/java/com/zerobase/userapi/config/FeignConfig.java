853b0eb9d35be7f944af090fed5b654580635df9
sunsik <sunsig21c@gmail.com>
2023-03-29 22:44:45 +0900

    update : mailgun api key 숨김

--- user-api/src/main/java/com/zerobase/userapi/config/FeignConfig.java
+++ user-api/src/main/java/com/zerobase/userapi/config/FeignConfig.java
@@ -2,14 +2,17 @@ package com.zerobase.userapi.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+	@Value("${mailgun.key}")
+	private String MAILGUN_KEY;
 	@Qualifier(value = "mailgun")
 	@Bean
 	public BasicAuthRequestInterceptor basicAuthenticationInterceptor() {
-		return new BasicAuthRequestInterceptor("api", "17160480aa5ccad5160c47590205d6e5-d51642fa-8fb7169d");
+		return new BasicAuthRequestInterceptor("api", MAILGUN_KEY);
 	}
 }
