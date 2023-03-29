aec15ef0cd2e4bb77d4baf3fa5b384cdf53e1fa8
sunsik <sunsig21c@gmail.com>
2023-03-29 18:04:50 +0900

    feat : multi module create

---
+++ user-api/src/test/java/com/zerobase/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
