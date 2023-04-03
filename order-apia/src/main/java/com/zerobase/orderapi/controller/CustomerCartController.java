a4bb041a414000025412092be3830dd7d9206a3a
sunsik <sunsig21c@gmail.com>
2023-04-03 20:33:57 +0900

    feat : [015-UPDATE_CART] 카트 업데이트
    -카트 조회 로직을 재사용하여 코드 생산성을 높혔음

--- order-apia/src/main/java/com/zerobase/orderapi/controller/CustomerCartController.java
+++ order-apia/src/main/java/com/zerobase/orderapi/controller/CustomerCartController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +35,13 @@ public class CustomerCartController {
 	public ResponseEntity<Cart> showCart(
 		@RequestHeader(name = "X-AUTH-TOKEN") String token) {
 		return ResponseEntity.ok(cartApplication.getCart(provider.getUserVo(token).getId()));
+	}
+
+	@PutMapping
+	public ResponseEntity<Cart> updateCart(
+		@RequestHeader(name = "X-AUTH-TOKEN") String token,
+		@RequestBody Cart cart) {
+
+		return ResponseEntity.ok(cartApplication.updateCart(provider.getUserVo(token).getId(), cart));
 	}
 }
