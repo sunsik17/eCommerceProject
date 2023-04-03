346f46a978d1a6a09c6ededc0a72e90997322e3a
sunsik <sunsig21c@gmail.com>
2023-04-03 15:51:20 +0900

    feat : [013-ADD_CART] 장바구니에 대한 추가 구현

--- order-apia/src/main/java/com/zerobase/orderapi/exception/ErrorCode.java
+++ order-apia/src/main/java/com/zerobase/orderapi/exception/ErrorCode.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 	SAME_ITEM_NAME(HttpStatus.BAD_REQUEST, "아이템 이름이 중복입니다."),
 	NOT_FOUND_ITEM(HttpStatus.BAD_REQUEST, "아이템을 찾을 수 없습니다."),
+	CART_CHANGE_FAIL(HttpStatus.BAD_REQUEST, "장바구니를 추가 할 수 없습니다."),
+	ITEM_COUNT_NOT_ENOUGH(HttpStatus.BAD_REQUEST, "상품의 수량이 부족합니다."),
 	NOT_FOUND_PRODUCT(HttpStatus.BAD_REQUEST, "상품을 찾을 수 없습니다.");
 	private final HttpStatus httpStatus;
 	private final String detail;
